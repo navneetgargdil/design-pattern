c0ea64dec95a177bc0fd72b36000e5dc32cd364f
Navneet Garg <navneet.garg@doitlean.com>
2025-01-03 11:17:32 +0530

    itrator pattern

--- BehaviouralPatterns/Iterator/Client.java
+++ BehaviouralPatterns/Iterator/Client.java
@@ -3,38 +3,47 @@ import java.util.Iterator;
 
 public class Client {
     public static void main(String[] args) {
+        if(args[0].equals("1")) {
+            System.out.println("Version 1");
+            BookCollectionV1 bookCollection = new BookCollectionV1();
+            bookCollection.addBook(new Book("C++ Book"));
+            bookCollection.addBook(new Book("Java Book"));
+            bookCollection.addBook(new Book("Python Book"));
+
+            for(int i=0;i<bookCollection.getBooks().size();i++){
+                System.out.println(bookCollection.getBooks().get(i));
+            }
+        }
         
-        BookCollectionV1 bookCollection = new BookCollectionV1();
-        bookCollection.addBook(new Book("C++ Book"));
-        bookCollection.addBook(new Book("Java Book"));
-        bookCollection.addBook(new Book("Python Book"));
+        if(args[0].equals("2")) {
+            System.out.println("Version 2");
 
-        for(int i=0;i<bookCollection.getBooks().size();i++){
-            System.out.println(bookCollection.getBooks().get(i));
+            BookCollectionV2 bookCollectionv2 = new BookCollectionV2();
+            bookCollectionv2.addBook(new Book("C++ Book"));
+            bookCollectionv2.addBook(new Book("Java Book"));
+            bookCollectionv2.addBook(new Book("Python Book"));
+
+            Iterator<Book> iteratorv2 = bookCollectionv2.createIterator();
+            while(iteratorv2.hasNext()){
+                Book book = iteratorv2.next();
+                System.out.println(book);
+            }
         }
 
-        BookCollectionV2 bookCollectionv2 = new BookCollectionV2();
-        bookCollectionv2.addBook(new Book("C++ Book"));
-        bookCollectionv2.addBook(new Book("Java Book"));
-        bookCollectionv2.addBook(new Book("Python Book"));
-
-        Iterator<Book> iteratorv2 = bookCollectionv2.createIterator();
-        while(iteratorv2.hasNext()){
-            Book book = iteratorv2.next();
-            System.out.println(book);
-        }
-
-        BookCollectionV3 bookCollectionv3 = new BookCollectionV3();
-        bookCollectionv3.addBook(new Book("Python Book"));
-        bookCollectionv3.addBook(new Book("C++ Book"));
-        bookCollectionv3.addBook(new Book("Java Book"));
+        if(args[0].equals("3")) {
+            System.out.println("Version 3");
+            BookCollectionV3 bookCollectionv3 = new BookCollectionV3();
+            bookCollectionv3.addBook(new Book("Python Book"));
+            bookCollectionv3.addBook(new Book("C++ Book"));
+            bookCollectionv3.addBook(new Book("Java Book"));
 
 
-        Iterator<Book> iteratorv3 = bookCollectionv3.iterator(); //Standardized
+            Iterator<Book> iteratorv3 = bookCollectionv3.iterator(); //Standardized
 
-        while(iteratorv3.hasNext()){
-            Book book = iteratorv3.next();
-            System.out.println(book);
+            while(iteratorv3.hasNext()){
+                Book book = iteratorv3.next();
+                System.out.println(book);
+            }
         }
 
     }
